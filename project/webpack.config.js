aec70e8d1f6aaf0da976016e3434fa45c89f3cfe
XuZhong.Chen <q20020628@163.com>
2018-08-10 14:06:49 +0800

    extract the same code.

--- project/webpack.config.js
+++ project/webpack.config.js
@@ -1,5 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // 这是package.json 中 dependencies下的
 const VENOR = [
@@ -20,11 +22,12 @@ module.exports = {
   entry: {
     // bundle和venor是自定义的，会映射到[name]中
     bundle : './src/index.js',
-    venor : VENOR
+    vendor : VENOR
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].js'
+    // [chunkhash]会根据文件是否改动而更换哈希
+    filename: '[name].[chunkhash].js'
   },
   module: {
     rules:[
@@ -37,5 +40,21 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      // mainfest文件是将每次打包都会更改的东西单独提取出来
+      // 保证没有更改的文件无需重新打包，这也可以加快打包速度
+      name: ['vendor','mainfest'],
+      // 配置mainfest文件使用
+      minChunks: Infinity
+    }),
+    new CleanWebpackPlugin(['dist/bundle.*.js','dist/mainfest.*.js'],{
+      verbose: true,
+      dry: false
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
 };
